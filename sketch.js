30dd4fbc6db74d91bd924329ea6642cb6e597751
khushi018238 <99384987+khushi018238@users.noreply.github.com>
2022-05-26 16:17:03 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var s;
+function setup() {
+  createCanvas(400,400);
+s=new Student("khushi",9,15);
+}
+
+function draw() 
+{
+  background(30);
+  s.display();
+}
---
+++ student.js
@@ -0,0 +1,17 @@
+
+class Student
+{
+ constructor(n,g,a){
+     this.name=n;
+     this.grade=g;
+     this.age=a;
+ } 
+ display(){
+     console.log(this.name);
+     console.log(this.grade);
+     console.log(this.age);
+ } 
+}
+
+
+
